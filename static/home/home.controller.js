7e2017f7f7ca4c18d4186fb623af66bba81acc86
Zhuoyuan Lin <winster1851@gmail.com>
2017-08-28 00:49:22 -0700

    add static files.

---
+++ static/home/home.controller.js
@@ -0,0 +1,46 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['UserService', '$rootScope'];
+  function HomeController(UserService, $rootScope) {
+    var vm = this;
+
+    vm.user = null;
+    vm.allUsers = [];
+    vm.deleteUser = deleteUser;
+
+    initController();
+
+    function initController() {
+      loadCurrentUser();
+      // loadAllUsers();
+    }
+
+    function loadCurrentUser() {
+      UserService.GetUserInfo()
+        .then(function (userData) {
+          console.log('userData', userData);
+          vm.user = userData.data;
+        });
+    }
+
+    function loadAllUsers() {
+      UserService.GetAll()
+        .then(function (users) {
+          vm.allUsers = users;
+        });
+    }
+
+    function deleteUser(id) {
+      UserService.Delete(id)
+        .then(function () {
+          loadAllUsers();
+        });
+    }
+  }
+
+})();
---
+++ static/login/login.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+              console.log(response.data);
+                if (response.data.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                    console.log('Login success!');
+                } else {
+                    FlashService.Error(response.data.message);
+                    vm.dataLoading = false;
+                }
+            });
+        }
+    }
+
+})();
